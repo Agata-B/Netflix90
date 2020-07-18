c4fc591046b5a2fafc6729345ef2aa4ca217c633
Agata <agatabienkowska1@wp.pl>
2020-07-18 12:58:39 +0200

    Change client address.

--- src/main/java/pl/yellowduck/netflix90/resources/client/Client.java
+++ src/main/java/pl/yellowduck/netflix90/resources/client/Client.java
@@ -1,19 +1,16 @@
 package pl.yellowduck.netflix90.resources.client;
 
+import pl.yellowduck.netflix90.resources.Person;
 
-import pl.yellowduck.netflix90.resources.Gender;
+public class Client extends Person {
+    private String Id;
+    private ClientAddress clientAddress;
+    private ClientStatus clientStatus;
 
-public class Client {
-    private String firstName;
-    private String lastName;
-    private final Gender gender;
-    ClientAddress clientAddress;
-    ClientStatus clientStatus;
 
-    public Client(String firstName, String lastName, Gender gender, ClientAddress clientAddress, ClientStatus clientStatus) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
+    public Client(String id, String firstName, String lastName, ClientAddress clientAddress, ClientStatus clientStatus) {
+        super(firstName, lastName);
+        this.Id = id;
         this.clientAddress = clientAddress;
         this.clientStatus = clientStatus;
     }
@@ -22,11 +19,27 @@ public class Client {
     public void printOutClient() {
         StringBuilder builder = new StringBuilder();
         builder.append("DANE KLIENTA ").append(" imię: ").append(firstName)
-                .append(" nzawisko: ").append(lastName)
-                .append(" płeć: ").append(gender)
+                .append(" nazwisko: ").append(lastName)
                 .append(" adres: ").append(clientAddress)
                 .append(" status klienta: ").append(clientStatus);
         System.out.println(builder.toString());
+    }
 
+
+    @Override
+    public void introduce() {
+    }
+
+
+    public void changeAddress(String street,
+             String homeNo,
+             String flatNo,
+             String city,
+             String postalCode){
+        clientAddress.setStreet(street);
+        clientAddress.setHomeNo(homeNo);
+        clientAddress.setFlatNo(flatNo);
+        clientAddress.setCity(city);
+        clientAddress.setPostalCode(postalCode);
     }
 }
--- src/main/java/pl/yellowduck/netflix90/resources/client/MainClient.java
+++ src/main/java/pl/yellowduck/netflix90/resources/client/MainClient.java
@@ -1,11 +1,12 @@
 package pl.yellowduck.netflix90.resources.client;
 
-import pl.yellowduck.netflix90.resources.Gender;
-
 public class MainClient {
     public static void main(String[] args) {
         ClientAddress clientAddress = new ClientAddress("Kolorowa", "5A", "2", "Płock", "00-000");
-        Client client = new Client("Ola", "Boś", Gender.FEMALE, clientAddress, ClientStatus.VIP);
+        Client client = new Client("001a", "Ola", "Boś", clientAddress, ClientStatus.VIP);
         client.printOutClient();
+        client.changeAddress("Biała", "41A", "2", "Gniezno", "11-222");
+        client.printOutClient();
+
     }
 }
